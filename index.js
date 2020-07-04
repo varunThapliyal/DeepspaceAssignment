7f1261afc666bd39d608747ddd08d30af900c893
unknown <vprakashthapliyal@gmail.com>
2020-07-04 23:28:08 +0530

    completed

---
+++ index.js
@@ -0,0 +1,141 @@
+import PRODUCT_DATA from './productData.js';
+import itemData from './itemData.js';
+console.log(PRODUCT_DATA);
+
+
+
+const renderProduct=(product)=>{
+    console.log(product.title);
+    const markup=`<div class="productOverview${product.title}" id=${product.title}>
+                      <h1 class="title">${product.title}</h1>    
+                  </div>
+                 `
+                     document.querySelector('.products').insertAdjacentHTML('beforeend',markup);
+
+                    product.items.map((item)=>{
+                   
+    const markupItem=`<div class="preview">
+  
+                         <div class='image'>
+                            <img class='Images' src=${item.imageUrl} alt="">
+                         </div>
+           
+                         <div class="productFooter">
+                            <span class="name">${item.name}</span>
+                            <span class="price">$ ${item.price}</span>
+                         </div>
+                         <a href="#${item.id}" >
+                            <button class='ghost' value=${item.id} }>More Details..</button>
+                         </a>
+
+                      </div>
+            
+            `;
+               document.querySelector(`.productOverview${product.title}`).insertAdjacentHTML('beforeend',markupItem);
+        
+          })
+}
+
+
+              
+               document.querySelector('.productDetails').style.display='none';
+               window.addEventListener('hashchange',()=>{
+
+               var hv=window.location.hash.replace('#','');
+               console.log(hv);
+                  if(hv===''||hv==='Hats'||hv==='Sneakers'||hv==='Jackets'||hv==='Women'||hv==='Men')
+                  {
+                     document.querySelector('.products').style.display='block';
+                     document.querySelector('.productDetails').style.display='none';
+                  }
+                  else
+                  {
+                     document.querySelector('.products').style.display='none';
+                     clearItem();
+                     const currentItem=itemData[hv];
+                     console.log(currentItem);
+                     renderCurrentItem(currentItem)
+                     document.querySelector('.productDetails').style.display='block';
+                  }
+      
+
+
+    
+})
+
+
+
+         PRODUCT_DATA.map((product)=>
+         {
+                    renderProduct(product)
+         })
+
+         const clearItem=()=>
+         {
+            document.querySelector('.productDetails').innerHTML='';
+         }
+
+         const renderCurrentItem =(item)=>{
+         const markUpItem=`<div class="productContainer" >
+                             <a href="#" class='Homepage'>Go back to Home Page</a>
+  
+                             <div class='itemImageDiv'>
+                                <img src=${item.imageUrl} alt="" class="itemImage">
+                             </div>
+                             <div class="productDescription">
+                                <h1 class="itemHeading">${item.name}</h1>
+                                <p class="itemPrice">$ ${item.price}</p>
+                                <p class="productDefinition">
+                                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                     It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </p>
+                                <a href=#${item.id} >
+                                   <button class='btn-cart'>Add To Cart</button>
+                                </a>
+                             </div>
+                          </div>`
+
+                          document.querySelector('.productDetails').insertAdjacentHTML("beforeend",markUpItem);
+                   }
+
+                   
+  
+      document.querySelector('.header').addEventListener('click',(e)=>{
+                   
+                   
+               
+               if(e.target.closest('.cartIcon')){
+                document.querySelector('.dropdown').classList.toggle('toggleVisible');
+
+               }
+      });
+              var quantity=0; 
+      document.querySelector('.productDetails').addEventListener('click',(e)=>{
+        
+              
+         if(e.target.closest('.btn-cart')){
+              const dItem=window.location.hash.replace('#','');
+              renderDropdown(itemData[dItem]);
+              quantity=quantity+1;
+              document.querySelector('.quantity').innerText=quantity;
+              console.log(quantity);
+              }
+        });
+
+      
+     const renderDropdown=(item)=>{
+             const markupDropdown = `<div class="dropdownItem">
+             <img src=${item.imageUrl} class="prodImage">
+             <p class='Pname'>${item.name}</p>
+          </div>`
+
+          document.querySelector('.dropdown').insertAdjacentHTML('beforeend',markupDropdown);
+     }
+
+
+        
+          
+                  
+                
---
+++ itemData.js
@@ -0,0 +1,126 @@
+const itemData ={
+    quantity:0,
+    1:{
+        id: 1,
+        name: 'Brown Brim',
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        price: 25
+      },
+     2: {
+        id: 2,
+        name: 'Blue Beanie',
+        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        price: 18
+      },
+      3:{
+        id: 3,
+        name: 'Brown Cowboy',
+        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+        price: 35
+      },
+     4:{
+        id: 4,
+        name: 'Grey Brim',
+        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        price: 25
+      },
+      5:{
+        id: 5,
+        name: 'Adidas NMD',
+        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        price: 220
+      },
+      6:{
+        id: 6,
+        name: 'Adidas Yeezy',
+        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+        price: 280
+      },
+      7:{
+        id: 7,
+        name: 'Black Converse',
+        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+        price: 110
+      },
+      8:{
+        id: 8,
+        name: 'Nike White AirForce',
+        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+        price: 160
+      },
+      9:{
+        id: 9,
+        name: 'Black Jean Shearling',
+        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        price: 125
+      },
+      10:{
+        id: 10,
+        name: 'Blue Jean Jacket',
+        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        price: 90
+      },
+      11:{
+        id: 11,
+        name: 'Grey Jean Jacket',
+        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        price: 90
+      },
+      12:{
+        id: 12,
+        name: 'Brown Shearling',
+        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+        price: 165
+      },
+      13:{
+        id: 13,
+        name: 'Blue Tanktop',
+        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+        price: 25
+      },
+      14:{
+        id: 14,
+        name: 'Floral Blouse',
+        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+        price: 20
+      },
+      15:{
+        id: 15,
+        name: 'Floral Dress',
+        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+        price: 80
+      },
+      16:{
+        id: 16,
+        name: 'Red Dots Dress',
+        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+        price: 80
+      },
+      17:{
+        id: 17,
+        name: 'Camo Down Vest',
+        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        price: 325
+      },
+      18:{
+        id: 18,
+        name: 'Floral T-shirt',
+        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+        price: 20
+      },
+      19:{
+        id: 19,
+        name: 'Black & White Longsleeve',
+        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+        price: 25
+      },
+      20:{
+        id: 20,
+        name: 'Pink T-shirt',
+        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+        price: 25
+      }
+
+}
+
+export default itemData;
